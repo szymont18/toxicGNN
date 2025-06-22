77a7d34f3d79e6bd2e9f33ff21aa2ac1c5b29ce8
Patryk Klatka <patryk.klatka@gmail.com>
2025-06-22 15:45:14 +0200

    feat: change model a bit

--- gnn/SimpleGNN.py
+++ gnn/SimpleGNN.py
@@ -1,25 +1,30 @@
 import torch
-import torch.nn.functional as F
+import torch.nn as nn
 from torch_geometric.nn import GCNConv, global_mean_pool
 
 
 class SimpleGNN(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_classes):
         super(SimpleGNN, self).__init__()
-        self.layer1 = GCNConv(input_dim, hidden_dim)
-        self.layer2 = GCNConv(hidden_dim, output_dim)
-        self.lin_layer = torch.nn.Linear(output_dim, num_classes)
+        self.conv1 = GCNConv(input_dim, hidden_dim)
+        self.conv2 = GCNConv(hidden_dim, output_dim)
+        
+        self.linear1 = nn.Linear(output_dim, output_dim)
+        self.dropout = nn.Dropout(0.05)
+        self.linear2 = nn.Linear(output_dim, num_classes)
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
-        x = self.layer1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, p=0.05, training=self.training)
-        x = self.layer2(x, edge_index)
-        x = F.relu(x)
-
+        
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        
         # Aggregate data from all nodes to one vector to represent whole graph
         x = global_mean_pool(x, batch)
+        
+        x = self.linear1(x).relu()
+        x = self.dropout(x)
+        x = self.linear2(x)
 
-        return self.lin_layer(x)
+        return x
 
